889bf39263d8d4181ea42a8affa649b55148884c
khian calderon <ckhianvictory@gmail.com>
2025-08-04 10:15:44 +0800

    Added basic form

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ export default function Home() {
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-semibold">Form Test</h1>
+        <form action="api/user">
+          <label htmlFor="form_name">Name: </label>
+          <input className="border-1 mt-2 rounded" id="form_name" name="form_name" type="text" />
+          <br/>
+          <input className="bg-blue-600 p-1 rounded-md mt-4 cursor-pointer" type="submit" value="Submit Name" />
+        </form>
       </div>
     </div>
   )
