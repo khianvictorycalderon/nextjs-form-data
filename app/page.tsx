ca93a25dd9196a6fccd6535c26c297c2fd4174f4
khian calderon <ckhianvictory@gmail.com>
2025-08-04 10:15:56 +0800

    Added method post

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-semibold">Form Test</h1>
-        <form action="api/user">
+        <form action="api/user" method="POST">
           <label htmlFor="form_name">Name: </label>
           <input className="border-1 mt-2 rounded" id="form_name" name="form_name" type="text" />
           <br/>
