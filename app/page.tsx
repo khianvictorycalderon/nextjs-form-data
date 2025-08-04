c4b16fe037af5a29904699021e384cb64713a379
khian calderon <ckhianvictory@gmail.com>
2025-08-04 10:11:26 +0800

    Added form test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold">Form Test</h1>
+      </div>
+    </div>
   )
 }
